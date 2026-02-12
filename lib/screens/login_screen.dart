f419d533f268df79e093c21763682001cccbe007
Emmanuel123159 <flakitomex12@gmail.com>
2026-02-12 10:54:57 -0600

    Feat(rive): wire up isCheking/isHandsUp inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,48 +9,103 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  // Control para mostrar/ocultar la contraseña
   bool _obscureText = true;
+
+  //Crear el cerebro de la animación
+  StateMachineController? _controller;
+  //SMI: State Machine Input
+  SMIBool? _isChecking;
+  SMIBool? _isHandsUp;
+  SMIBool? _trigSuccess;
+  SMIBool? _trigFail;
 
   @override
   Widget build(BuildContext context) {
+    // Para obtener el tamaño de la pantalla
     final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      //Evita que se quite espacio del nudge
       body: SafeArea(
+        // Evita que el contenido se superponga con el notch
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
             children: [
               SizedBox(
-                width: size.width,
-                height: 200,
+                width:size.width,
+                height:200,
                 child: RiveAnimation.asset(
                   'assets/animated_login_bear.riv',
-                  fit: BoxFit.contain,
+                  stateMachines: ['Login Machine'],
+                  //Al inciar la anaimación
+                  onInit: (artboard){
+                    _controller = StateMachineController.fromArtboard(artboard, 
+                    'Login Machine',
+                    );
+
+                    //Verifica que inició bien
+                    if (_controller == null);
+                    //Agrega el controlador al tablero/escenario
+                    artboard.addController(_controller!);
+                    //Vincular variables
+                    _isChecking = _controller!.findSMI('isChecking');
+                    _isHandsUp = _controller!.findSMI('isHandsUp');
+                    _trigSuccess = _controller!.findSMI('trigSuccess');
+                    _trigFail = _controller!.findSMI('trigFail');
+                  },
+                  )
                 ),
-              ),
-              const SizedBox(height: 24),
+              // Para separación
+              const SizedBox(height: 10),
+
               TextField(
+                onChanged:(value) {
+                  if (_isHandsUp != null){
+                     _isHandsUp!.change(false);
+
+                }
+                if (_isChecking == null ) return;
+                 _isChecking!.change(true);
+ 
+
+                },
                 decoration: InputDecoration(
                   hintText: 'Email',
                   prefixIcon: const Icon(Icons.email),
                   border: OutlineInputBorder(
+                    // Para redondear los bordes
                     borderRadius: BorderRadius.circular(12),
                   ),
                 ),
               ),
-              const SizedBox(height: 12),
+
+              const SizedBox(height: 10),
+
               TextField(
+                onChanged:(value) {
+                  if (_isChecking != null){
+                     _isChecking!.change(false);
+
+                }
+                if (_isHandsUp == null ) return;
+                 _isHandsUp!.change(true);
+ 
+
+                },
                 obscureText: _obscureText,
                 decoration: InputDecoration(
                   hintText: 'Password',
                   prefixIcon: const Icon(Icons.lock),
                   suffixIcon: IconButton(
                     icon: Icon(
-                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                      _obscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility,
                     ),
                     onPressed: () {
+                      //Refresca el icono
                       setState(() {
                         _obscureText = !_obscureText;
                       });
@@ -61,7 +116,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              const SizedBox(height: 16),
             ],
           ),
         ),
